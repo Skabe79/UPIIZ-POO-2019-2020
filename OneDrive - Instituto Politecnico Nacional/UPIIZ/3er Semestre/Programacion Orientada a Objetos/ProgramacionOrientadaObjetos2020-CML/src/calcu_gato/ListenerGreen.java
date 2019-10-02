d27d5f719a4945641e8d3600e4c7edc5bc736a8c
Carlos Montes <skabe79@gmail.com>
2019-10-02 12:52:50 -0500

    02/10/2019 12:51 Iniciando la creaccion de ListenerGreen

---
+++ OneDrive - Instituto Politecnico Nacional/UPIIZ/3er Semestre/Programacion Orientada a Objetos/ProgramacionOrientadaObjetos2020-CML/src/calcu_gato/ListenerGreen.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcu_gato;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ *
+ * @author Carlos Montes Leyva
+ */
+public class ListenerGreen implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+}
